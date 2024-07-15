4caa87f70c86d76fb39e1aa9de5c26bced18f01c
snehalkadwe <snehalkadwe@gmail.com>
2024-07-15 20:38:20 +0530

    added code to fetch data

--- app/Livewire/UploadImage.php
+++ app/Livewire/UploadImage.php
@@ -4,6 +4,8 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Http;
+
 
 
 class UploadImage extends Component
@@ -19,5 +21,62 @@ class UploadImage extends Component
     public function uploadImage()
     {
         dd($this->file);
+        $question = "what is moon?";
+        $info = $this->sendRequest($question);
+    }
+
+    public function sendRequest($question)
+    {
+        $authorizationToken = config('chatbot-configs.cloudflare.api_key');
+        $accountId = config('chatbot-configs.cloudflare.account_id');
+        $url = 'https://api.cloudflare.com/client/v4/accounts/' . $accountId . '/ai/run/@cf/meta/llama-2-7b-chat-fp16';
+        // $response = Http::get('http://example.com');
+        $response = Http::withHeaders([
+            'Authorization' => "Bearer $authorizationToken",
+            'Content-Type' => 'application/json',
+        ])
+            ->post($url, [
+                'json' => [
+                    'messages' => [
+                        [
+                            'role' => 'system',
+                            'content' => 'You are GalaxyBot, a knowledgeable cosmic companion who provides detailed and accurate information related to galaxies, stars, and the mysteries of the universe. You deliver information in a concise, factual manner without including greetings or unnecessary introductions.'
+                        ],
+                        [
+                            'role' => 'user',
+                            'content' => 'The user has asked for information about a specific star or galaxy. Provide detailed and accurate information in the following format:\n\n1. Name and Classification: \n2. Key Characteristics: \n3. Historical Significance: \n4. Interesting Facts: \n\nHere is the query: ' . $question
+                        ]
+                    ]
+                ]
+            ]);
+
+
+
+        // $client = new Client();
+
+
+
+        // $response = $client->post($url, [
+        //     'headers' => [
+        //         'Authorization' => "Bearer $authorizationToken",
+        //         'Content-Type' => 'application/json',
+        //     ],
+        //     'json' => [
+        //         'messages' => [
+        //             [
+        //                 'role' => 'system',
+        //                 'content' => 'You are GalaxyBot, a knowledgeable cosmic companion who provides detailed and accurate information related to galaxies, stars, and the mysteries of the universe. You deliver information in a concise, factual manner without including greetings or unnecessary introductions.'
+        //             ],
+        //             [
+        //                 'role' => 'user',
+        //                 'content' => 'The user has asked for information about a specific star or galaxy. Provide detailed and accurate information in the following format:\n\n1. Name and Classification: \n2. Key Characteristics: \n3. Historical Significance: \n4. Interesting Facts: \n\nHere is the query: ' . $question
+        //             ]
+        //         ]
+        //     ]
+        // ]);
+
+        $responseBody = json_decode($response->getBody(), true);
+
+        return  $responseBody['result']['response'];
     }
 }
